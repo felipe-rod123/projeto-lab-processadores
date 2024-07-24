3248cd77ca19594923c77b44506564c89ed70ff5
lontra-lontra <ianribeiroleite@usp.br>
2024-07-24 17:26:22 -0300

    feat: codigo recomendado pelo professor

---
+++ src/defines.h
@@ -0,0 +1,126 @@
+/********************************************************************
+Copyright 2010-2017 K.C. Wang, <kwang@eecs.wsu.edu>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+NOTE: This header file is modified from its original contents to 
+	  facilitate the implementation of an interrupt-driven UART
+	  driver.
+********************************************************************/
+
+// defines.h file
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+// General type definitions
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+// GPIO 
+#define GPIO_BASE	0x20200000
+#define SYSTEM_TIMER_BASE	0x20003000
+
+#define GPIO_GPFSEL0    0
+#define GPIO_GPFSEL1    1
+#define GPIO_GPFSEL2    2
+#define GPIO_GPFSEL3    3
+#define GPIO_GPFSEL4    4
+#define GPIO_GPFSEL5    5
+
+#define GPIO_GPSET0     7
+#define GPIO_GPSET1     8
+
+#define GPIO_GPCLR0     10
+#define GPIO_GPCLR1     11
+
+#define GPIO_GPLEV0     13
+#define GPIO_GPLEV1     14
+
+#define GPIO_GPEDS0     16
+#define GPIO_GPEDS1     17
+
+#define GPIO_GPREN0     19
+#define GPIO_GPREN1     20
+
+#define GPIO_GPFEN0     22
+#define GPIO_GPFEN1     23
+
+#define GPIO_GPHEN0     25
+#define GPIO_GPHEN1     26
+
+#define GPIO_GPLEN0     28
+#define GPIO_GPLEN1     29
+
+#define GPIO_GPAREN0    31
+#define GPIO_GPAREN1    32
+
+#define GPIO_GPAFEN0    34
+#define GPIO_GPAFEN1    35
+
+#define GPIO_GPPUD      37
+#define GPIO_GPPUDCLK0  38
+#define GPIO_GPPUDCLK1  39
+
+// Peripheral base address
+#define PERIPHERAL_BASE 0x20000000UL
+
+// UART
+#define UDR   0x00
+#define UDS   0x04
+#define RXFE  0x10
+#define UFR   0x18
+#define TXFF  0x20
+#define CNTL  0x2C
+#define IMSC  0x38
+#define MIS   0x40
+#define SBUFSIZE 128
+
+// I2C
+#define I2C_BASE 0x20804000
+#define I2C_CONTROL 0
+#define I2C_STATUS 1
+#define I2C_DLEN 2
+#define I2C_SLAVE_ADDRESS 3
+#define DATA_FIFO 4
+#define CLOCK_DIVIDER 5
+#define DATA_DELAY 6
+#define CLOCK_STRETCH_TIMEOUT 7
+
+#define I2C_C_I2CEN    (1 << 15)
+#define I2C_C_INTR    (1 << 10)
+#define I2C_C_INTT    (1 << 9)
+#define I2C_C_INTD    (1 << 8)
+#define I2C_C_ST    (1 << 7)
+#define I2C_C_CLEAR    (1 << 4)
+#define I2C_C_READ    1
+
+#define START_READ    I2C_C_I2CEN|I2C_C_ST|I2C_C_CLEAR|I2C_C_READ
+#define START_WRITE    I2C_C_I2CEN|I2C_C_ST
+
+#define I2C_S_CLKT    (1 << 9)
+#define I2C_S_ERR    (1 << 8)
+#define I2C_S_RXF    (1 << 7)
+#define I2C_S_TXE    (1 << 6)
+#define I2C_S_RXD    (1 << 5)
+#define I2C_S_TXD    (1 << 4)
+#define I2C_S_RXR    (1 << 3)
+#define I2C_S_TXW    (1 << 2)
+#define I2C_S_DONE    (1 << 1)
+#define I2C_S_TA    1
+
+// Interrupt register
+#define IRQ2 0x85
+
+#endif
